a15d4179836ce2bf1bc5e1fc3bb9eb38b22551f6
tinchoa <martinandreoni@gmail.com>
2018-01-23 11:02:30 +0100

    create-suricata
    file to create suricata output from a pcap file (could be used with several files in arquivos)

---
+++ scripts-tratamento/create-suricata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+path=$(pwd)
+pathlogs=/usr/local/var/log/suricata
+arquivo=$path/new2
+
+while read -r line
+do
+        f="$line"
+        echo 'analizing file '$F
+        v2=${f/.}
+        mkdir $v2
+        suricata -c /usr/local/etc/suricata/suricata.yaml -r $f -l $v2
+        cp $f $v2
+#       mv $pathlogs/* $v2
+
+done < "$arquivo"
