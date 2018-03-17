e06da1ebc258412271ac1bd7b168771a8c4296f9
root <root@sepetiba.gta.ufrj.br>
2018-03-17 10:12:40 -0300

    update strip

--- scripts-tratamento/strip_ppoe.sh
+++ scripts-tratamento/strip_ppoe.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+#before running must be created 2 folders 
+# nPPoe where the output of the stripe will go
+# pass where the file without strip will go
+
 FILES=$1 #passig the file location by argument
 pathstrip=/home/stripe
 #netAI-rules-stats-ni.xml
@@ -9,4 +13,5 @@ do
    file="${f##*/}" # Atribui o valor da variável file ao nome do arquivo "dump.pcapxxx"
    echo 'analizing file '$file
    $pathstrip/stripe -r $f -w $FILES/nPPoe/$file # Remove o PPoE do arquivo dump.pcapxxx e
+   mv $FILES/$f $FILES/pass
 done                                       #envia o(s) arquivo(s) a pasta com o mesmo nome do                                           #determinado pcap em análise   
