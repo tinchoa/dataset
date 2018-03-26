e01609a0b283e63f4122acba2f264a5b8054e1b6
tinchoa <martinandreoni@gmail.com>
2018-03-26 17:33:34 +0200

    dia 14

--- figuras/dataset-movel/carnaval-2018/day-14/fig7-flowDuration-bytes/fig7a.m
+++ figuras/dataset-movel/carnaval-2018/day-14/fig7-flowDuration-bytes/fig7a.m
@@ -1,9 +1,9 @@
 figure
-durationAttack = csvread('saidaFlowSizeFor.csv');
+durationAttack = csvread('fig7a-saidaFlowSizeFor.csv');
 [f,x] = ecdf(durationAttack(:,2));
 plot(f,'r')
 hold on
-durationNormal = csvread('saidaFlowSizeBack.csv');
+durationNormal = csvread('fig7a-saidaFlowSizeBack.csv');
 [f,x] = ecdf(durationNormal(:,2));
 plot(f,'g')
 xlim([0 100])
