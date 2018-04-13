254ef2aa97e73a332693f7f27325ea4fe0636eab
tinchoa <martinandreoni@gmail.com>
2018-04-13 19:47:22 +0200

    up

---
+++ figuras/dataset-fixo/carnaval-2018/porcentagem-ameaças-por-dia/relationAtackNormal.m
@@ -0,0 +1,22 @@
+figure
+formatSpec = '%s%f%f%f%f%[^\n\r]';
+delimiter = ',';
+
+fileID = fopen('porcentage.csv','r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter,  'ReturnOnError', false);
+fclose(fileID);
+
+day = dataArray{:, 1};
+day=['16/2';'17/2';'18/2';'19/2';'20/2';'21/2';'24/2';'25/2';'26/2';'27/2';'28/2';'01/3';'02/3';'03/3'];
+totalFlows = dataArray{:, 2};
+ataques = dataArray{:, 3};
+normal = dataArray{:, 4};
+porcentageAttacks = dataArray{:, 5};
+%bar(porcentageAttacks)
+bar([ataques totalFlows],'stack')
+set(gca,'xticklabel',day)
+%ylabel('Percentage of Threats');
+ylabel('Number of Flows');
+legend('Alerts', 'Normal Traffic')
+%ylim([0 40])
+xtickangle(45)
