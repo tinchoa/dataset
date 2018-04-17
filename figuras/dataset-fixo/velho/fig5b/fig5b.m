611c9d2c77301fa035a4a3d1f2ecfad72713f299
tinchoa <martinandreoni@gmail.com>
2018-04-17 16:50:05 +0200

    up

--- figuras/dataset-fixo/velho/fig5b/fig5b.m
+++ figuras/dataset-fixo/velho/fig5b/fig5b.m
@@ -2,6 +2,8 @@ figure
 protoAttack = csvread('fig5b-saidaprotoAttacknew.csv');
 protoNormal = csvread('fig5b-saidaprotoNormalnew.csv');
 values=[protoNormal(1,2),protoAttack(1,2);protoNormal(2,2),protoAttack(2,2)];
-bar(values);
+x=bar(values);
 set(gca,'xticklabel',{'TCP';'UDP'})
 ylabel('Number of Flows');
+hatchfill2(x(1),'cross','HatchAngle',45);
+hatchfill2(x(2),'single','HatchAngle',0);
